0125c99aa99f2002581651d2f796adabf9c4b05e
Laboratoria-183 <you@example.com>
2019-06-03 19:07:14 -0500

    establecer funcion para comenzar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("hola"); 
+//llamar boton de comenzar//
+document.getElementById("start").addEventListener("click",comenzar);
+//ejecutar funcion de comenzar//
+function comenzar(){
+    let firstpage = document.getElementById("start").style.visibility= "visible";
+}
