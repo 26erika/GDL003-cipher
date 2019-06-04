bc8c9e53731ec7bff2184878f64f0049f5b876c9
Laboratoria-183 <you@example.com>
2019-06-04 14:35:05 -0500

    boton comenzar funciona

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+//ocultar segunda pagina
+    document.getElementById("two").style.visibility="hidden";
 //llamar boton de comenzar//
-document.getElementById("start").addEventListener("click",comenzar);
+    document.getElementById("start").addEventListener("click",comenzar);
 //ejecutar funcion de comenzar//
 function comenzar(){
-    let enter = document.getElementById("start").style.visibility= "hidden";
-    document.getElementById("two").addEventListener.style.visibility="visible";
+    let enter = 
+    document.getElementById("root").style.display= "none";
+    document.getElementById("one").style.display= "none";
+    document.getElementById("two").style.visibility="visible";
 }
+
