20e84dbfb0dc445c64d787f294a9d98c50acaeb4
Laboratoria-183 <you@example.com>
2019-06-04 19:01:25 -0500

    agregue funcion apara cifrar

--- src/index.js
+++ src/index.js
@@ -9,4 +9,19 @@ function comenzar(){
     document.getElementById("one").style.display= "none";
     document.getElementById("two").style.visibility="visible";
 }
+//definir alfabeto
+let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// desplazamiento a la derecha
+encodeOffset = "right";
+// desplazamiento a la izquierda
+decodeOffset = "left";
+//constantes para llamar funcion
+const encode = document.getElementById("encode");
+const decode = document.getElementById("decode");
+const displayEncode = document.getElementById ("normaltext");
+//llamar funcion al presionar botones
+encode.addEventListener(click,function(){
+    displayEncode.innerHTML=window.cipher.encode();
+});
+
 
